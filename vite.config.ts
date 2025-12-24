0d0c7df29b18a8469bc53ae80718e5c351af5e0f
ecletika <83987553+ecletika@users.noreply.github.com>
2025-12-24 18:06:32 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/NotaFiscal_Marcia_Rita/",
+  // ⚡ Ajuste base para caminhos relativos no GitHub Pages
+  base: "./",
+  
   server: {
     host: "::",
     port: 8080,
   },
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
